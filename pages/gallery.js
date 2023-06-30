ae3e58364000b2f72622c2c6c1fa7601865749d0
joshuakalu0 <ajujoshua2019@gmail.com>
2023-06-30 15:08:04 +0100

    de

--- pages/gallery.js
+++ pages/gallery.js
@@ -7,8 +7,8 @@ import useSplitarray from "../components/hooks/useSplitarray";
 import useFetch from "../components/hooks/useFetch";
 import axios from "axios";
 
-export default function Gallery({ catalogs }) {
-  const record = JSON.parse(catalogs);
+export default function Gallery(props) {
+  const record = JSON.parse(props);
   const route = useRouter();
   const [isloading, setisloading] = useState(false);
   const [watch, setwatch] = useState({
@@ -24,42 +24,47 @@ export default function Gallery({ catalogs }) {
 
   const [result, setpage, seturl] = useFetch(url, isloading);
 
-  useEffect(() => {
-    splitter(record);
-  }, [result]);
-  useEffect(() => {
-    console.log(watch);
-  }, [watch]);
-  useEffect(() => {
-    if (hit == true) {
-      splitter(result);
-      setisloading(true);
-    }
-  }, [hit]);
+  // useEffect(() => {
+  //   splitter(record);
+  // }, [result]);
+  // useEffect(() => {
+  //   console.log(watch);
+  // }, [watch]);
+  // useEffect(() => {
+  //   if (hit == true) {
+  //     splitter(result);
+  //     setisloading(true);
+  //   }
+  // }, [hit]);
 
   return (
     <div className='relative'>
-      <Images array={watch} />
+      {/* <Images array={watch} />
       <div className='w-full flex justify-center items-center'>
         {isloading && <CircularProgress />}
-      </div>
+      </div> */}
     </div>
   );
 }
 
 export async function getStaticProps() {
   const url = `https://fervencciD.onrender.com/api/v1/catalog?limit=10`;
-  try {
-    const catalog = await axios.get(url);
-    console.log(catalog, "d");
-    return {
-      props: {
-        catalogs: JSON.stringify(catalog),
-      },
-    };
-  } catch (err) {
-    return {
-      notFound: true,
-    };
-  }
+  return axios
+    .get(url)
+    .then((res) => {
+      const data = res.data;
+      return {
+        props: {
+          data: JSON.stringify(data),
+        },
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+      return {
+        props: {
+          err: JSON.stringify(err),
+        },
+      };
+    });
 }
